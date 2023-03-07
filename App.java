cb8bb0621ecb8c1af2177aa272cf05f30e629dd3
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-03-06 22:44:18 -0300

    Update code

--- App.java
+++ App.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class App{
 
     public static void main(String[] args){
 
-        
+        try (Scanner scan = new Scanner(System.in)) {
+            int scoreStudent = -1;
+
+            while(scoreStudent < 0 || scoreStudent > 10){
+
+                System.out.println("Digite a nota do aluno: ");
+                scoreStudent = scan.nextInt();
+                if (scoreStudent < 0 || scoreStudent > 10) System.out.println("Nota inválida!");
+
+            }
+
+            System.out.println("Nota do aluno: " + scoreStudent);
+        }
+
     }
 
 }
